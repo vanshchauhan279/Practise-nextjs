757875338c96ec4b787da84ac4942856a119bc11
vansh <chauhanvansh279@gmail.com>
2025-07-04 22:12:20 +0530

    useEffect handleScreen Width

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    images: {
-    domains: ['encrypted-tbn2.gstatic.com'], // Add the domain here
+    domains: ['encrypted-tbn0.gstatic.com'], // Add the domain here
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
-import Cardss from "./components/Cardss";
+"use client"
+
+import React from "react";
+import { useState,useEffect } from "react";
 
 
-export default function Home() {
- 
+const MainHead = () => {
+
+const [screenwidth,setScreenwidth] = useState(window.innerWidth)
+
+  
+  useEffect(()=>{
+      const handleWidthchange = () => setScreenwidth(window.innerWidth);
+      
+      window.addEventListener('resize',handleWidthchange)
+      console.log("chm")
+  },[])
+
   return (
-    <div className="flex p-3 items-center justify-center  font-bold text-2xl capitalize mt-5">
-         <Cardss/>   
+    <div className="flex justify-center items-center flex-col">
+
+      <h1>Width of my Screen : {screenwidth}</h1>
+      
     </div>
   );
-}
+};
+
+export default MainHead;
